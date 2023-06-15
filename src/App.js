04ab31fbb1282088a8dfb23eb631450379356623
zawadi <zawadirugenyi@gmail.com>
2023-06-15 15:49:10 +0300

    COMMIT

--- src/App.js
+++ src/App.js
@@ -4,32 +4,32 @@ import Expenses from "./components/Expenses";
 function App() {
   const expenses = [
     {
-      id: 'e1',
-      title: 'Donation', 
-      amount: 12954.67, 
-      date: new Date(2021, 4, 9)
+        id: 'e1',
+        title: 'Donation', 
+        amount: 12954.67, 
+        date: new Date(2021, 4, 9)
     },
 
     {
-      id: 'e2',
-      title: 'Car Insurance',
-      amount: 294.67, 
-      date: new Date(2018, 0, 28)
+        id: 'e2',
+        title: 'Car Insurance',
+        amount: 294.67, 
+        date: new Date(2018, 0, 28)
     },
 
     {
-      id: 'e3',
-      title: ' Clothes shopping', 
-      amount: 294.67, 
-      date: new Date(2020, 12, 17)
+        id: 'e3',
+        title: ' Clothes shopping', 
+        amount: 294.67, 
+        date: new Date(2020, 12, 17)
     },
 
     {
-      id: 'e4',
-      title: ' Savings ', 
-      amount: 30094.67, 
-      date: new Date(2023, 6, 28)
-    },
+        id: 'e4',
+        title: ' Savings ', 
+        amount: 30094.67, 
+        date: new Date(2023, 6, 28)
+      },
   ];
   
   return (
