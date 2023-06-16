2bdf790b53d63187f53d5080e6221924173822f0
zawadi <zawadirugenyi@gmail.com>
2023-06-16 16:39:53 +0300

    commit

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -7,7 +7,7 @@ function Expenses(props){
 
 return <Card className="expenses">
 
-  <ExpenseItem 
+   <ExpenseItem 
       title={props.items[0].title} 
       amount={props.items[0].amount} 
       date={props.items[0].date} 
